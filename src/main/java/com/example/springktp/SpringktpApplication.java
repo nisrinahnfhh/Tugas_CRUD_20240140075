22a21d32fd328d88cead7a79abe01ed59e9c5f23
Nisrina Hanifah Ramadhani <nisrinahnfhh@gmail.com>
2026-03-14 15:50:05 +0000

    Membuat setup awal project

---
+++ src/main/java/com/example/springktp/SpringktpApplication.java
@@ -0,0 +1,13 @@
+package com.example.springktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringktpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringktpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springktp/SpringktpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringktpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
