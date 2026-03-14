5d34cdc30a8572fbfdb587213855da2759f844b6
Nisrina Hanifah Ramadhani <nisrinahnfhh@gmail.com>
2026-03-14 16:02:00 +0000

    membuat model

---
+++ src/main/java/com/example/springktp/model/WebResponse.java
@@ -0,0 +1,15 @@
+package com.example.springktp.model;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WebResponse<T> {
+    private int code;
+    private String status;
+    private T data;
+}
